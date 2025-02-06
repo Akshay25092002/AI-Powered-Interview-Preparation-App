2758b906eafa66c1f874623ed894e0fc372e941d
AK Vankar <25sid999@gmail.com>
2025-02-06 23:59:33 +0530

    first commit

---
+++ frontend-ai-powered-interview-preparation-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="text-blue-500">
+      <Button variant={"outline"}>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
